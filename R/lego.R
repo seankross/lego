26505b89527996eee456a33fbc440e13f53c0dfa
William <willtheorangeguy@users.noreply.github.com>
2017-12-18 13:39:41 -0800

    Update lego.r in conformance with LEGO brand guidelines.

--- R/lego.R
+++ R/lego.R
@@ -1,6 +1,6 @@
-#' @title Lego set data 1970-2015
+#' @title LEGO set data 1970-2015
 #' @name lego
-#' @description Data for every lego set produced from 1970 to 2015 including number
+#' @description Data for every LEGO set produced from 1970 to 2015 including number
 #' of pieces per set, number of minifigures per set, and region specific MSRP.
 #' 
 #' @seealso \code{\link{legosets}}
